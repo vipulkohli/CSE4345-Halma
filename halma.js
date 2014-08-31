2e4f70f761529ccc620635f713c96c509fa35ced
JessicaYeh <jyeh@smu.edu>
2014-08-30 22:05:15 -0500

    replaced tabs with spaces and fixed indenting

--- halma.js
+++ halma.js
@@ -27,12 +27,11 @@ function getCursorPosition(e) {
     var x;
     var y;
     if (e.pageX != undefined && e.pageY != undefined) {
-	x = e.pageX;
-	y = e.pageY;
-    }
-    else {
-	x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-	y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        x = e.pageX;
+        y = e.pageY;
+    } else {
+        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
     }
     x -= gCanvasElement.offsetLeft;
     y -= gCanvasElement.offsetTop;
@@ -45,44 +44,44 @@ function getCursorPosition(e) {
 function halmaOnClick(e) {
     var cell = getCursorPosition(e);
     for (var i = 0; i < gNumPieces; i++) {
-	if ((gPieces[i].row == cell.row) && 
-	    (gPieces[i].column == cell.column)) {
-	    clickOnPiece(i);
-	    return;
-	}
+        if ((gPieces[i].row == cell.row) && (gPieces[i].column == cell.column)) {
+            clickOnPiece(i);
+            return;
+        }
     }
     clickOnEmptyCell(cell);
 }
 
 function clickOnEmptyCell(cell) {
-    if (gSelectedPieceIndex == -1) { return; }
+    if (gSelectedPieceIndex == -1) {
+        return;
+    }
     var rowDiff = Math.abs(cell.row - gPieces[gSelectedPieceIndex].row);
     var columnDiff = Math.abs(cell.column - gPieces[gSelectedPieceIndex].column);
-    if ((rowDiff <= 1) &&
-	(columnDiff <= 1)) {
-	/* we already know that this click was on an empty square,
-	   so that must mean this was a valid single-square move */
-	gPieces[gSelectedPieceIndex].row = cell.row;
-	gPieces[gSelectedPieceIndex].column = cell.column;
-	gMoveCount += 1;
-	gSelectedPieceIndex = -1;
-	gSelectedPieceHasMoved = false;
-	drawBoard();
-	return;
+    if ((rowDiff <= 1) && (columnDiff <= 1)) {
+        /* we already know that this click was on an empty square,
+           so that must mean this was a valid single-square move */
+        gPieces[gSelectedPieceIndex].row = cell.row;
+        gPieces[gSelectedPieceIndex].column = cell.column;
+        gMoveCount += 1;
+        gSelectedPieceIndex = -1;
+        gSelectedPieceHasMoved = false;
+        drawBoard();
+        return;
     }
     if ((((rowDiff == 2) && (columnDiff == 0)) ||
-	 ((rowDiff == 0) && (columnDiff == 2)) ||
-	 ((rowDiff == 2) && (columnDiff == 2))) && 
-	isThereAPieceBetween(gPieces[gSelectedPieceIndex], cell)) {
-	/* this was a valid jump */
-	if (!gSelectedPieceHasMoved) {
-	    gMoveCount += 1;
-	}
-	gSelectedPieceHasMoved = true;
-	gPieces[gSelectedPieceIndex].row = cell.row;
-	gPieces[gSelectedPieceIndex].column = cell.column;
-	drawBoard();
-	return;
+     ((rowDiff == 0) && (columnDiff == 2)) ||
+     ((rowDiff == 2) && (columnDiff == 2))) && 
+    isThereAPieceBetween(gPieces[gSelectedPieceIndex], cell)) {
+        /* this was a valid jump */
+        if (!gSelectedPieceHasMoved) {
+            gMoveCount += 1;
+        }
+        gSelectedPieceHasMoved = true;
+        gPieces[gSelectedPieceIndex].row = cell.row;
+        gPieces[gSelectedPieceIndex].column = cell.column;
+        drawBoard();
+        return;
     }
     gSelectedPieceIndex = -1;
     gSelectedPieceHasMoved = false;
@@ -90,7 +89,9 @@ function clickOnEmptyCell(cell) {
 }
 
 function clickOnPiece(pieceIndex) {
-    if (gSelectedPieceIndex == pieceIndex) { return; }
+    if (gSelectedPieceIndex == pieceIndex) {
+        return;
+    }
     gSelectedPieceIndex = pieceIndex;
     gSelectedPieceHasMoved = false;
     drawBoard();
@@ -102,29 +103,29 @@ function isThereAPieceBetween(cell1, cell2) {
     var rowBetween = (cell1.row + cell2.row) / 2;
     var columnBetween = (cell1.column + cell2.column) / 2;
     for (var i = 0; i < gNumPieces; i++) {
-	if ((gPieces[i].row == rowBetween) &&
-	    (gPieces[i].column == columnBetween)) {
-	    return true;
-	}
+        if ((gPieces[i].row == rowBetween) &&
+            (gPieces[i].column == columnBetween)) {
+            return true;
+        }
     }
     return false;
 }
 
 function isTheGameOver() {
     for (var i = 0; i < gNumPieces; i++) {
-	if (gPieces[i].row > 2) {
-	    return false;
-	}
-	if (gPieces[i].column < (kBoardWidth - 3)) {
-	    return false;
-	}
+        if (gPieces[i].row > 2) {
+            return false;
+        }
+        if (gPieces[i].column < (kBoardWidth - 3)) {
+            return false;
+        }
     }
     return true;
 }
 
 function drawBoard() {
     if (gGameInProgress && isTheGameOver()) {
-	endGame();
+        endGame();
     }
 
     gDrawingContext.clearRect(0, 0, kPixelWidth, kPixelHeight);
@@ -133,14 +134,14 @@ function drawBoard() {
     
     /* vertical lines */
     for (var x = 0; x <= kPixelWidth; x += kPieceWidth) {
-	gDrawingContext.moveTo(0.5 + x, 0);
-	gDrawingContext.lineTo(0.5 + x, kPixelHeight);
+        gDrawingContext.moveTo(0.5 + x, 0);
+        gDrawingContext.lineTo(0.5 + x, kPixelHeight);
     }
     
     /* horizontal lines */
     for (var y = 0; y <= kPixelHeight; y += kPieceHeight) {
-	gDrawingContext.moveTo(0, 0.5 + y);
-	gDrawingContext.lineTo(kPixelWidth, 0.5 +  y);
+        gDrawingContext.moveTo(0, 0.5 + y);
+        gDrawingContext.lineTo(kPixelWidth, 0.5 +  y);
     }
     
     /* draw it! */
@@ -148,7 +149,7 @@ function drawBoard() {
     gDrawingContext.stroke();
     
     for (var i = 0; i < 9; i++) {
-	drawPiece(gPieces[i], i == gSelectedPieceIndex);
+        drawPiece(gPieces[i], i == gSelectedPieceIndex);
     }
 
     gMoveCountElem.innerHTML = gMoveCount;
@@ -168,30 +169,30 @@ function drawPiece(p, selected) {
     gDrawingContext.strokeStyle = "#000";
     gDrawingContext.stroke();
     if (selected) {
-	gDrawingContext.fillStyle = "#000";
-	gDrawingContext.fill();
+        gDrawingContext.fillStyle = "#000";
+        gDrawingContext.fill();
     }
 }
 
 if (typeof resumeGame != "function") {
     saveGameState = function() {
-	return false;
+        return false;
     }
     resumeGame = function() {
-	return false;
+        return false;
     }
 }
 
 function newGame() {
     gPieces = [new Cell(kBoardHeight - 3, 0),
-	       new Cell(kBoardHeight - 2, 0),
-	       new Cell(kBoardHeight - 1, 0),
-	       new Cell(kBoardHeight - 3, 1),
-	       new Cell(kBoardHeight - 2, 1),
-	       new Cell(kBoardHeight - 1, 1),
-	       new Cell(kBoardHeight - 3, 2),
-	       new Cell(kBoardHeight - 2, 2),
-	       new Cell(kBoardHeight - 1, 2)];
+           new Cell(kBoardHeight - 2, 0),
+           new Cell(kBoardHeight - 1, 0),
+           new Cell(kBoardHeight - 3, 1),
+           new Cell(kBoardHeight - 2, 1),
+           new Cell(kBoardHeight - 1, 1),
+           new Cell(kBoardHeight - 3, 2),
+           new Cell(kBoardHeight - 2, 2),
+           new Cell(kBoardHeight - 1, 2)];
     gNumPieces = gPieces.length;
     gSelectedPieceIndex = -1;
     gSelectedPieceHasMoved = false;
@@ -208,12 +209,12 @@ function endGame() {
 function initGame(canvasElement, moveCountElement) {
     if (!canvasElement) {
         canvasElement = document.createElement("canvas");
-	canvasElement.id = "halma_canvas";
-	document.body.appendChild(canvasElement);
+        canvasElement.id = "halma_canvas";
+        document.body.appendChild(canvasElement);
     }
     if (!moveCountElement) {
         moveCountElement = document.createElement("p");
-	document.body.appendChild(moveCountElement);
+        document.body.appendChild(moveCountElement);
     }
     gCanvasElement = canvasElement;
     gCanvasElement.width = kPixelWidth;
@@ -222,6 +223,6 @@ function initGame(canvasElement, moveCountElement) {
     gMoveCountElem = moveCountElement;
     gDrawingContext = gCanvasElement.getContext("2d");
     if (!resumeGame()) {
-	newGame();
+        newGame();
     }
 }
