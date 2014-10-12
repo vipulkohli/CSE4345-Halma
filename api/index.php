f13b74bcf18725df5fa1f0ff170cf41cfc60a5bb
JessicaYeh <jyeh@smu.edu>
2014-10-11 19:29:31 -0500

    added phpDoc style comments

--- api/index.php
+++ api/index.php
@@ -1,16 +1,26 @@
 <?php 
+// Jessica Yeh
+// http://lyle.smu.edu/~jyeh/4345/api/index.php/getMove
+
 // Uses the Slim PHP REST Framework: http://slimframework.com/
 require 'Slim/Slim.php';
 $app = new Slim();
-
 $app->post('/getMove', 'getMove');
-
 $app->run();
 
-// Class to encapsulate X and Y coordinates
+/**
+ * Cell -- Encapsulates X and Y coordinates and a generic marker flag.
+ * @author Jessica Yeh <jyeh@smu.edu>
+ */
 class Cell {
     public $x, $y, $used;
 
+    /**
+     * Constructor for Cell. Sets variables.
+     * @param int $x x-coordinate
+     * @param int $y y-coordinate
+     * @param bool $used marker flag
+     */
     function Cell($x = 0, $y = 0, $used = false) {
         $this->x = $x;
         $this->y = $y;
@@ -18,22 +28,41 @@ class Cell {
     }
 }
 
-// Class to encapsulate the list of points in a path, and helper functions
+/**
+ * Path -- Encapsulates the list of points in a path, and helper functions.
+ * @author Jessica Yeh <jyeh@smu.edu>
+ */
 class Path {
     public $cells = array();
 
+    /**
+     * Constructor for Path. Adds the first point in the path.
+     * @param Cell $start The first point in the path.
+     */
     function Path($start) {
         $this->addCell($start);
     }
 
+    /**
+     * Returns the last point in the path.
+     * @return Cell
+     */
     function getLastCell() {
         return end((array_values($this->cells)));
     }
 
+    /**
+     * Returns the first point in the path.
+     * @return Cell
+     */
     function getFirstCell() {
         return $this->cells[0];
     }
 
+    /**
+     * Returns the second to last point in the path.
+     * @return Cell
+     */
     function getPreviousCell() {
         if (count($this->cells) < 2) {
             return NULL;
@@ -42,21 +71,30 @@ class Path {
         }
     }
 
+    /**
+     * Adds a point to the end of the path.
+     * @param Cell $cell The point to add to the path.
+     */
     function addCell($cell) {
         array_push($this->cells, $cell);
     }
 }
 
 /**
- * Compares two numbers $a and $b
- * @return if $a > $b, return 1; if $a < $b, return -1; if $a = $b, return 0
+ * Compares two numbers, $a and $b.
+ * @param float $a The left-hand-side number to compare.
+ * @param float $b The right-hand-side number to compare.
+ * @return bool if $a > $b, return 1; if $a < $b, return -1; if $a = $b, return 0
  */
 function compare($a, $b) {
     return ($a == $b) ? 0 : (($a > $b) ? 1 : -1);
 }
 
 /**
- * HW 10: Halma AI v1.1
+ * Given JSON post data with the Halma game board info, return a JSON string
+ * with info on the best move to make.
+ * @return string A JSON string with the best move in the format:
+ * {"from": {"x": 22, "y": 30}, "to": [{"x": 24, "y": 30}, {"x": 26, "y": 30}]}
  */
 function getMove() {
     // Parse the JSON input
@@ -68,12 +106,16 @@ function getMove() {
     // Pick a destination cell from the target area
     for ($i = 0; $i < count($target); $i++) {
         $destination = getTopRightDestination($target);
+
+        // If a piece is in the destination cell, set some flags to mark as used
         foreach ($pieces as &$piece) {
             if (areCellsEqual($destination, $piece)) {
                 $piece->used = false;
                 $destination->used = true;
             }
         }
+
+        // Stop looping if an empty destination cell is found
         if (!$destination->used) {
             break;
         }
@@ -136,22 +178,30 @@ function getMove() {
     // Determine the best move to make
     $path = getBestPath($finishedMovePaths, $destination);
 
-    // Print out the chosen best move as JSON in the format:
-    // {"from": {"x": 22, "y": 30}, "to": [{"x": 24, "y": 30}, {"x": 26, "y": 30}]}
+    // Setup the arrays to hold "from" and "to" info    
     $from = array('x' => $path->getFirstCell()->x, 'y' => $path->getFirstCell()->y);
     $to = array();
+
+    // Add path points to the "to" array
     for ($i = 1; $i < count($path->cells); $i++) {
         $toMove = array('x' => $path->cells[$i]->x, 'y' => $path->cells[$i]->y);
         array_push($to, $toMove);
     }
-    $move = array('from' => $from, 'to' => $to);
-    echo json_encode($move);
+
+    // Print out the chosen best move as JSON
+    echo json_encode(array('from' => $from, 'to' => $to));
 }
 
-// Turn a JSON array of cells into an array of cell objects.
+/**
+ * Turn a JSON array of cells into an array of cell objects.
+ * @param JSONObject[] $jsonCells An array of JSON objects with coordinates.
+ * @param bool $used Whether to mark each of the Cells as "used".
+ * @return Cell[]
+ */
 function decodePieces($jsonCells, $used = true) {
     $arrayCells = array();
 
+    // Create a Cell object for each cell in the JSON array
     foreach ($jsonCells as $jsonCell) {
         array_push($arrayCells, new Cell($jsonCell->x, $jsonCell->y, $used));
     }
@@ -159,17 +209,28 @@ function decodePieces($jsonCells, $used = true) {
     return $arrayCells;
 }
 
-// Calculate the distance between two cells.
+/**
+ * Calculate the distance between two cells.
+ * @param Cell $loc1 One point in the distance calculation.
+ * @param Cell $loc2 The other point in the distance calculation.
+ * @return float The distance between $loc1 and $loc2.
+ */
 function distanceBetweenCells($loc1, $loc2) {
     return sqrt(pow($loc2->x - $loc1->x, 2) + pow($loc2->y - $loc1->y, 2));
 }
 
-// For a given list of destination cells, return the top-right-most empty cell.
+/**
+ * For a given list of destination cells, return the top-right-most empty cell.
+ * @param Cell[] $cells The array of destination cells.
+ * @return Cell The top-right-most empty (used is false) cell.
+ */
 function getTopRightDestination($cells) {
+    // TODO: fix this to not be hard coded
     $topRightCorner = new Cell(8, 0);
     $minDistance = PHP_INT_MAX;
     $topRightCell = NULL;
 
+    // Determine the Cell that is closest to the top-right corner
     foreach ($cells as $cell) {
         $distance = distanceBetweenCells($cell, $topRightCorner);
         if ($distance < $minDistance && !$cell->used) {
@@ -181,12 +242,20 @@ function getTopRightDestination($cells) {
     return $topRightCell;
 }
 
-// Given a $movingPiece to move, return a list of all the possible Cells that
-// the $movingPiece can move to, either by jumping or not jumping.
-// $allPieces is a list of Cells with pieces, and $boardSize is the number
-// of rows/columns in the board (assumes a square board).
-// $previousLocation is the Cell that the $movingPiece had been in previously,
-// and is used to make sure the piece doesn't infinitely move back and forth.
+
+/**
+ * Given a $movingPiece to move, return a list of all the possible Cells that
+ * the $movingPiece can move to, either by jumping or not jumping.
+ * $allPieces is a list of Cells with pieces, and $boardSize is the number
+ * of rows/columns in the board (assumes a square board).
+ * $previousLocation is the Cell that the $movingPiece had been in previously,
+ * and is used to make sure the piece doesn't infinitely move back and forth.
+ * @param Cell $movingPiece The piece that is moving.
+ * @param Cell[] $allPieces All of the pieces on the board.
+ * @param int $boardSize The number of rows/columns in the board.
+ * @param Cell $previousCell The previous location (if any) of the $movingPiece.
+ * @return Cell[] A list of Cells that the $movingPiece can move to.
+ */
 function getPossibleSingleMovesFromPiece($movingPiece, $allPieces, $boardSize, $previousCell = NULL) {
     $possibleMoves = array();
 
@@ -221,7 +290,7 @@ function getPossibleSingleMovesFromPiece($movingPiece, $allPieces, $boardSize, $
                 $x = $movingPiece->x + $xDiff*2;
                 $y = $movingPiece->y + $yDiff*2;
                 $jumpCell = new Cell($x, $y, false);
-                
+
                 if (!isCellXYInList($jumpCell, $allPieces) && 
                     isCellInsideBoard($jumpCell, $boardSize) &&
                     !areCellsEqual($jumpCell, $previousCell)) {
@@ -234,8 +303,13 @@ function getPossibleSingleMovesFromPiece($movingPiece, $allPieces, $boardSize, $
     return $possibleMoves;
 }
 
-// Return true if the given Cell's coordinates matches the coordinates of
-// a piece in the $pieceList. Otherwise, return false.
+/**
+ * Return true if the given Cell's coordinates matches the coordinates of
+ * a piece in the $pieceList. Otherwise, return false.
+ * @param Cell $cell The Cell we're checking to see if it's in the list.
+ * @param Cell[] $cell The list of pieces from which to search for the Cell.
+ * @return bool
+ */
 function isCellXYInList($cell, $pieceList) {
     foreach ($pieceList as $piece) {
         if ($piece->x == $cell->x && $piece->y == $cell->y) {
@@ -245,7 +319,12 @@ function isCellXYInList($cell, $pieceList) {
     return false;
 }
 
-// Return true if the Cell's (x, y) coordinates are inside the board boundaries.
+/**
+ * Return true if the Cell's (x, y) coordinates are inside the board boundaries.
+ * @param Cell $cell The Cell that we are checking.
+ * @param int $boardSize Number of rows/columns of the game board.
+ * @return bool
+ */
 function isCellInsideBoard($cell, $boardSize) {
     if (($cell->x >= 0 && $cell->x <= $boardSize-1) && 
         ($cell->y >= 0 && $cell->y <= $boardSize-1)) {
@@ -254,7 +333,12 @@ function isCellInsideBoard($cell, $boardSize) {
     return false;
 }
 
-// Return true if the (x, y) of $cellA matches the (x, y) of $cellB
+/**
+ * Return true if the (x, y) of $cellA matches the (x, y) of $cellB
+ * @param Cell $cellA The first cell to compare.
+ * @param Cell $cellB The second cell to compare.
+ * @return bool
+ */
 function areCellsEqual($cellA, $cellB) {
     if ($cellA != NULL && $cellB != NULL) {
         if ($cellA->x == $cellB->x && $cellA->y == $cellB->y) {
@@ -264,14 +348,22 @@ function areCellsEqual($cellA, $cellB) {
     return false;
 }
 
-// Return the best path out of the list of paths to reach the destination
+/**
+ * Return the best path out of the list of paths to reach the destination
+ * @param Path[] $paths The list of Paths to choose from.
+ * @param Cell $destination The destination Cell.
+ * @return Path
+ */
 function getBestPath($paths, $destination) {
     $maxDistance = -1;
     $bestPath = NULL;
 
+    // Iterate through the $paths to find the best $path
     foreach ($paths as $path) {
         $pathLength = distanceBetweenCells($path->getFirstCell(), $path->getLastCell());
         $distanceToDestination = distanceBetweenCells($path->getLastCell(), $destination);
+
+        // Prevent divide-by-0 error when calculating the best path
         if ($distanceToDestination != 0) {
             $optimalDistance = $pathLength / $distanceToDestination;
             if ($optimalDistance > $maxDistance) {
